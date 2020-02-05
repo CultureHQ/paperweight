6a3be9abe86862f6944293e0cc428ab91bdb7b6a
Kevin Deisz <kevin.deisz@gmail.com>
2020-02-05 15:01:03 -0500

    Clean up mergify

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,9 @@
 ENV['RAILS_ENV'] = 'test'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'paperweight'
